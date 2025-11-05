789636239f5a38a5c36a0a85f0202678c8d15085
Nicolas <nikolasalp@gmail.com>
2025-11-05 16:10:13 -0300

    add: controllers

---
+++ Controllers/WishlistController.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using story_brook_api.Data;
+using story_brook_api.Models;
+
+namespace story_brook_api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WishlistController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public WishlistController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Wishlist
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Wishlist>>> GetWishlist()
+        {
+            return await _context.Wishlist.ToListAsync();
+        }
+
+        // GET: api/Wishlist/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Wishlist>> GetWishlist(string id)
+        {
+            var wishlist = await _context.Wishlist.FindAsync(id);
+
+            if (wishlist == null)
+            {
+                return NotFound();
+            }
+
+            return wishlist;
+        }
+
+        // PUT: api/Wishlist/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutWishlist(string id, Wishlist wishlist)
+        {
+            if (id != wishlist.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(wishlist).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!WishlistExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Wishlist
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Wishlist>> PostWishlist(Wishlist wishlist)
+        {
+            _context.Wishlist.Add(wishlist);
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateException)
+            {
+                if (WishlistExists(wishlist.Id))
+                {
+                    return Conflict();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return CreatedAtAction("GetWishlist", new { id = wishlist.Id }, wishlist);
+        }
+
+        // DELETE: api/Wishlist/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteWishlist(string id)
+        {
+            var wishlist = await _context.Wishlist.FindAsync(id);
+            if (wishlist == null)
+            {
+                return NotFound();
+            }
+
+            _context.Wishlist.Remove(wishlist);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool WishlistExists(string id)
+        {
+            return _context.Wishlist.Any(e => e.Id == id);
+        }
+    }
+}
