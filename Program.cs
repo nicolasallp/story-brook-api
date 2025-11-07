7416745ac94ebb794165af9afb466a2e67d6fe2d
Nicolas <nikolasalp@gmail.com>
2025-11-07 14:12:19 -0300

    update: cors policy

--- Program.cs
+++ Program.cs
@@ -6,7 +6,6 @@ using Microsoft.Extensions.Options;
 using story_brook_api.Data;
 
 var builder = WebApplication.CreateBuilder(args);
-builder.WebHost.UseUrls("http://0.0.0.0:5278", "https://0.0.0.0:7073");
 
 // Add services to the container.
 
@@ -20,21 +19,16 @@ builder.Services.AddDbContext<AppDbContext>(options =>
 
 builder.Services.AddCors(options =>
 {
-    options.AddPolicy("AllowLocalhost", policy =>
-    {
-        policy.WithOrigins(
-            "http://localhost:5173",
-            "http://127.0.0.1:5173"
-        )
-        .AllowAnyHeader()
-        .AllowAnyMethod()
-        .AllowCredentials();
-    });
+    options.AddPolicy("AllowAll",
+        policy => policy
+            .AllowAnyOrigin()
+            .AllowAnyMethod()
+            .AllowAnyHeader());
 });
 
 var app = builder.Build();
 
-app.UseCors("AllowLocalhost");
+app.UseCors("AllowAll");
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
