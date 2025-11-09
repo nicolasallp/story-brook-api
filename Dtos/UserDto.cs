e03084879fb1920317fe82485f940de5cfd0b3b0
Nicolas <nikolasalp@gmail.com>
2025-11-09 20:34:06 -0300

    update: user DTO

--- Dtos/UserDto.cs
+++ Dtos/UserDto.cs
@@ -6,4 +6,9 @@
         public string? Email { get; set; }
         public string? Password { get; set; }
     }
+
+    public class UserUpdateDto
+    {
+        public string? Password { get; set; }
+    }
 }
